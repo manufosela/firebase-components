06805bd8da14b528c86edeb21390544526301b3f
manufosela <manufosela@gmail.com>
2026-01-18 12:07:06 +0100

    Add Astro catalog and Pages workflow

---
+++ apps/site/src/data/site.js
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Firebase Components",
+  description: "Firebase-ready web components with live demos and integration notes.",
+  repoUrl: "https://github.com/manufosela/firebase-components",
+  basePath: "/firebase-components/",
+};
